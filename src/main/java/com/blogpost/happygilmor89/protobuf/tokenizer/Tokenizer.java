e1917d38d1c652e2d67daeb700d47811e956946d
dobrovolskiy <happygilmor89@gmail.com>
2014-12-15 00:59:21 +0300

    Improved tokenizer: got all field descriptors of builder

--- src/main/java/com/blogpost/happygilmor89/protobuf/tokenizer/Tokenizer.java
+++ src/main/java/com/blogpost/happygilmor89/protobuf/tokenizer/Tokenizer.java
@@ -4,12 +4,11 @@ import com.blogpost.happygilmor89.protobuf.tutoral.AddressBookProtos;
 import com.google.protobuf.Descriptors;
 
 import java.util.HashSet;
-import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 
 /**
  * Created by HG89 on 14.12.2014.
+ *
  */
 public class Tokenizer {
     public static Set<Descriptors.FieldDescriptor> tokenize(com.google.protobuf.GeneratedMessage.Builder builder) {
@@ -26,10 +25,6 @@ public class Tokenizer {
             }
         }
         return descriptorSet;
-    }
-
-    public static String tokenize(Descriptors.FieldDescriptor descriptor) {
-        return descriptor.getName() + " ";
     }
 
     public static void main(String[] args) {
